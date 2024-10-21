5d990f35533951ea51988e523b33474511689062
Richard Shea <rshea@thecubagroup.com>
2024-10-22 10:39:39 +1300

    Linter driven cleanups

--- counter.py
+++ counter.py
@@ -8,10 +8,15 @@ from nanodjango import Django
 
 app = Django()
 
+
 @app.admin
 class CountLog(models.Model):
     # Standard Django model, registered with the admin site
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.timestamp
+
 
 @app.route("/")
 def count(request):
@@ -19,11 +24,13 @@ def count(request):
     CountLog.objects.create()
     return f"<p>Number of page loads: {CountLog.objects.count()}</p>"
 
+
 @app.api.get("/add")
 def add(request):
     # Django Ninja API support built in
     CountLog.objects.create()
     return {"count": CountLog.objects.count()}
+
 
 @app.route("/slow/")
 async def slow(request):
--- todos.py
+++ todos.py
@@ -1,8 +1,10 @@
 '''
 A example of using ninjadjango to provide a 'one-file' Django project
-which exposes an API. 
+which exposes an API.
 
 The API allows for items on 'to-do' list to be maintained and reviewed
 by the consumer of the API
 '''
+from nanodjango import Django
 
+app = Django()
