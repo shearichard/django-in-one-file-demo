513b28fb7a1675e71244cf7b5e36a4e481806110
Richard Shea <rshea@thecubagroup.com>
2024-10-22 10:06:46 +1300

    Stub of todos.py

---
+++ todos.py
@@ -0,0 +1,8 @@
+'''
+A example of using ninjadjango to provide a 'one-file' Django project
+which exposes an API. 
+
+The API allows for items on 'to-do' list to be maintained and reviewed
+by the consumer of the API
+'''
+
